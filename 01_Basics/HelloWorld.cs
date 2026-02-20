9a593dcde20da6e99bb26f65299580007bf395ea
Kajal-Ranvir <kailasranvir438@gmail.com>
2026-02-20 15:40:12 +0530

    Added first 3 C# programs

---
+++ 01_Basics/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharp
+{
+    public class HelloWorld
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
