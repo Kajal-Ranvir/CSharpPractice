1602b09636e1bf62c26c6ce0ec37661595b1ea8d
Kajal-Ranvir <kajalranvir944@gmail.com>
2026-04-26 15:51:24 +0530

    Implemented 164 xUnit testing with proper project structure and passing test

---
+++ Level_04_Enterprise/164_xUnit_Testing/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        var calc = new Calculator();
+
+        int result = calc.Add(2, 3);
+
+        Assert.Equal(5, result);
+    }
+}
