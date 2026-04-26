05f583718a698d81113cfc6b6486152d70554217
Kajal-Ranvir <kajalranvir944@gmail.com>
2026-04-26 14:58:48 +0530

    Implemented 162 Configuration management using appsettings.json

--- Level_04_Enterprise/162_AppSettings_Config/Program.cs
+++ Level_04_Enterprise/162_AppSettings_Config/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        var config = builder.Build();
+
+        string appName = config["AppSettings:AppName"];
+        string version = config["AppSettings:Version"];
+        int number = Convert.ToInt32(config["AppSettings:DefaultNumber"]);
+
+        Console.WriteLine($"App Name: {appName}");
+        Console.WriteLine($"Version: {version}");
+        Console.WriteLine($"Default Number: {number}");
+    }
+}
