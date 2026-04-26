eea4f77618e931ca94b83c27eec3e26020857fb2
Kajal-Ranvir <kajalranvir944@gmail.com>
2026-04-26 14:51:17 +0530

    Implemented 161 Logging system using Serilog

---
+++ Level_04_Enterprise/161_Logging_Serilog/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Serilog;
+
+class Program
+{
+    static void Main()
+    {
+        // Logger config
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        try
+        {
+            Log.Information("Application Started");
+
+            Console.Write("Enter number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Log.Information($"User entered: {num}");
+
+            int result = 100 / num;
+
+            Console.WriteLine($"Result: {result}");
+            Log.Information("Calculation successful");
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Error occurred");
+            Console.WriteLine("Something went wrong!");
+        }
+        finally
+        {
+            Log.Information("Application Ended");
+            Log.CloseAndFlush();
+        }
+    }
+}
