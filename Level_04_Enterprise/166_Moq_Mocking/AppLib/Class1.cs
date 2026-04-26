2a343b4da2cdbc18c848b3f37cf83efe7c8bac46
Kajal-Ranvir <kajalranvir944@gmail.com>
2026-04-26 16:38:00 +0530

    Implemented 166 Moq mocking with proper namespace and dependency testing

---
+++ Level_04_Enterprise/166_Moq_Mocking/AppLib/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace AppLib
+{
+    public interface IMessageService
+    {
+        void SendMessage();
+    }
+
+    public class Notification
+    {
+        private readonly IMessageService _service;
+
+        public Notification(IMessageService service)
+        {
+            _service = service;
+        }
+
+        public void Notify()
+        {
+            _service.SendMessage();
+        }
+    }
+}
---
+++ Level_04_Enterprise/166_Moq_Mocking/AppTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using AppLib;
+using Xunit;
+using Moq;
+
+public class Tests
+{
+    [Fact]
+    public void Notify_ShouldCallSendMessage()
+    {
+        var mock = new Mock<IMessageService>();
+
+        var notification = new Notification(mock.Object);
+
+        notification.Notify();
+
+        mock.Verify(x => x.SendMessage(), Times.Once);
+    }
+}
