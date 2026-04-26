35448ac0ee69d7ea201ac8da913d189b7bafcfb4
Kajal-Ranvir <kajalranvir944@gmail.com>
2026-04-26 16:02:11 +0530

    Implemented 165 NUnit testing with Calculator example

---
+++ Level_04_Enterprise/165_NUnit_Testing/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+public class Tests
+{
+    [Test]
+    public void Add_ReturnsCorrectSum()
+    {
+        var calc = new Calculator();
+
+        int result = calc.Add(2, 3);
+
+        Assert.AreEqual(5, result);
+    }
+}
