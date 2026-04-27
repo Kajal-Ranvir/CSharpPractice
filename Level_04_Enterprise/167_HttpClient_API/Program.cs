ba1fb25935320e69191f4e6b5b650a51394f121e
Kajal-Ranvir <kajalranvir944@gmail.com>
2026-04-27 20:54:09 +0530

    Implemented 167 HttpClient API call using async await

--- Level_04_Enterprise/167_HttpClient_API/Program.cs
+++ Level_04_Enterprise/167_HttpClient_API/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using HttpClient client = new HttpClient();
+
+        string url = "https://jsonplaceholder.typicode.com/posts/1";
+
+        try
+        {
+            string response = await client.GetStringAsync(url);
+
+            Console.WriteLine("API Response:");
+            Console.WriteLine(response);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
